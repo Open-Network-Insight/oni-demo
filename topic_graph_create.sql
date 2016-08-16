34a5d574e5c9fb2ba9cebd5f2f790f6e2828db67
natedogs911 <natedogs911@gmail.com>
2016-08-16 13:25:43 -0700

    removing old code

--- topic_graph_create.sql
+++
@@ -1,6 +0,0 @@
-SELECT firstseensrcip, firstseendestip, firstseensrcport, firstseendestport, 
-AVG(firstseensrcpayloadbytes) as avgfirstsrcbytes, AVG(firstseendestpayloadbytes) as avgfirstdstbytes, 
-AVG(firstseensrctotalbytes) as avgtotalsrcbytes, AVG(firstseendesttotalbytes) as avgtotaldstbytes, 
-AVG(firstseensrcpacketcount) as avgfirstsrcpkts, AVG(firstseendestpacketcount) as avgfirstdstpkts
-FROM vast_netflow
-GROUP BY firstseensrcip, firstseendestip, firstseensrcport, firstseendestport
--- vast2hdfs.sh
+++
@@ -1,9 +0,0 @@
-mkdir vast
-cd vast
-mkdir nf
-# move vast data to hdfs
-hadoop fs -mkdir /user/hadoop/netflow/
-hadoop fs -mkdir /user/hadoop/netflow/vast/
-hadoop fs -put /home/hadoop/ipython/vast/nf/nf-* /user/hadoop/netflow/vast/nf/.
- hadoop fs -ls /user/hadoop/netflow/vast/nf/
-
